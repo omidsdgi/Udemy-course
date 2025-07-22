a0e72e6de7d92a9b81de1711655311ce7c6bcf79
omidsdgi <omid69sdgi@gmail.com>
2025-07-22 14:34:53 +0330

    add global Enter key handler using ref to focus input and clear query

--- eat-n-split/eat-split/src/components/usePopcorn/Search.tsx
+++ eat-n-split/eat-split/src/components/usePopcorn/Search.tsx
@@ -1,8 +1,23 @@
- interface SearchProps {
+import {useEffect, useRef} from "react";
+
+interface SearchProps {
     query: string;
     setQuery: (query: string) => void;
 }
 export function Search({query,setQuery}:SearchProps) {
+    const inputEl = useRef<HTMLInputElement>(null);
+
+    useEffect( ()=>  {
+        function callback(e:KeyboardEvent){
+            if(e.code === "Enter"){
+                if(document.activeElement === inputEl.current) return
+            inputEl.current?.focus();
+            setQuery("");
+            }
+        }
+            document.addEventListener("keydown", callback);
+            return ()=>document.removeEventListener("keydown", callback);
+    }, [setQuery]);
     return (
             <input
                 className="search"
@@ -10,6 +25,7 @@ export function Search({query,setQuery}:SearchProps) {
                 placeholder="Search movies..."
                 value={query}
                 onChange={(e) => setQuery(e.target.value)}
+                ref={inputEl}
             />
     );
 }
